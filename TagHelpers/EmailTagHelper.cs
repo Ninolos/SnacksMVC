798e222f9ec044f216acf9920a55defcfa2ca225
Jonathan Daniel Loth <ninoloth@gmail.com>
2022-04-12 20:07:12 -0300

    Tag Helper
    create a Tag Helper for the Contact link, now is possible send an Email for contact

---
+++ TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SnackMVC.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string Adress { get; set; }
+        public string Content { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + Adress);
+            output.Content.SetContent(Content);
+        }
+    }
+}
