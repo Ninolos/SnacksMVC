3471f5e52cc4df676ec9b632bc207f7d5e30892f
Jonathan Daniel Loth <ninoloth@gmail.com>
2022-04-14 12:28:02 -0300

    Add Login logic Controller/ViewModel

---
+++ Controllers/AcoountController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SnackMVC.ViewModels;
+
+namespace SnackMVC.Controllers
+{
+    public class AcoountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AcoountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid)
+                return View(loginVM);
+
+            //get the user
+            var user = await _userManager.FindByEmailAsync(loginVM.UserName);
+
+            if (user != null)
+            {
+                //try make the login
+                var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+                if (result.Succeeded) 
+                {              
+                    if (string.IsNullOrEmpty(loginVM.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(loginVM.ReturnUrl);
+                }
+            }
+
+            ModelState.AddModelError("", "Fail to try the Login");
+            return View(loginVM);
+        }
+    }
+}
---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SnackMVC.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Inform Name")]
+        [Display(Name ="User")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Inform Password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Passwprd")]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
