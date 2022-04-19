38b9dc6ec49e9701ae62df4d7517eb2b55ee339b
Jonathan Daniel Loth <ninoloth@gmail.com>
2022-04-18 21:41:20 -0300

    Logout Implement

--- Controllers/AcoountController.cs
+++ Controllers/AcoountController.cs
@@ -78,6 +78,14 @@ namespace SnackMVC.Controllers
             }
             return View(registerVM);
         }
-        
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            HttpContext.Session.Clear();
+            HttpContext.User = null;
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
